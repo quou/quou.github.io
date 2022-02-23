cc365d12d8ebe1a79b576b77ce7ce596292503a1
George Lamb <georgeashleylamb@gmail.com>
2022-02-23 21:08:35 +1100

    Make pages responsive

--- gen.lua
+++ gen.lua
@@ -2,6 +2,8 @@ html_start = [[
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="UTF-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 <title>veridis_quo_t</title>
 <style>
 body {
